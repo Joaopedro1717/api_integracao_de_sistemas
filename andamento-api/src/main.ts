8b9b720c312e4022ac9b3a51d493e735ddb68408
kauan <kauanalexandre2003@outlook.com>
2025-05-05 01:40:03 -0300

    API ANDAMENTO finalizado

--- andamento-api/src/main.ts
+++ andamento-api/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
+  app.enableCors();
 
-  await app.listen(port);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
   
   console.log(`Servidor rodando no localhost:${port}`);
 }
